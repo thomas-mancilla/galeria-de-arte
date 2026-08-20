e58697d23f24b60d6072777195f0d1b5f6bedceb
elito-hub <elitouwu@gmail.com>
2026-08-19 22:44:28 -0400

    Cambio de Artista y Bodega. Adicion de metodos getters, setters y constructores con validacion.

--- src/main/java/com/mycompany/galeria/de/arte/Bodega.java
+++ src/main/java/com/mycompany/galeria/de/arte/Bodega.java
@@ -1,5 +1,21 @@
 package com.mycompany.galeria.de.arte;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bodega {
-    
+
+    private Map<String, Obra> inventario; // La clave es el ID de la obra.
+
+    public Bodega() {
+        this.inventario = new HashMap<>();
+    }
+
+    public Map<String, Obra> getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(Map<String, Obra> inventario) {
+        this.inventario = inventario;
+    }
 }
--- src/main/java/com/mycompany/galeria/de/arte/Exhibicion.java
+++ src/main/java/com/mycompany/galeria/de/arte/Exhibicion.java
@@ -1,5 +1,13 @@
 package com.mycompany.galeria.de.arte;
 
 public class Exhibicion {
-    
+    private String nombre;
+    private String tematica;
+    private ArrayList<Obra> obrasExhibidas;
+
+    public Exhibicion(String nombre, String tematica) {
+        this.nombre = nombre;
+        this.tematica = tematica;
+        this.obrasExhibidas = new ArrayList<>();
+    }
 }
