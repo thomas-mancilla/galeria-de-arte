7e7c1e58d935f5ba17143541a9d913d6cda0b223
Mister Luxz <misterluxz@gmail.com>
2026-09-11 16:21:54 -0300

    Implement gallery loading and saving
    Added persistence functionality to load and save galleries.

--- src/main/java/com/mycompany/galeria/de/arte/Main.java
+++ src/main/java/com/mycompany/galeria/de/arte/Main.java
@@ -4,8 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         Galeria galeria = new Galeria();
-        ConsolaMenu menu = new ConsolaMenu(galeria);
+        Persistencia persistencia = new Persistencia();
 
+        if (!persistencia.cargarSalas(galeria)) {
+            System.out.println(
+                "No se pudieron cargar las salas. "
+                + "Revise salas.txt antes de continuar."
+            );
+            return;
+        }
+
+        ConsolaMenu menu = new ConsolaMenu(galeria);
         menu.mostrarMenu();
+
+        if (persistencia.guardarSalas(galeria)) {
+            System.out.println("Salas guardadas correctamente.");
+        } else {
+            System.out.println("Error: no se pudieron guardar las salas.");
+        }
     }
 }
