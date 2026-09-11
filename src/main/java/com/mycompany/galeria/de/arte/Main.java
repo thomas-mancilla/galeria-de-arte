e89663164be6f49570063d08472e8815f71a17cc
Mister Luxz <misterluxz@gmail.com>
2026-09-11 08:44:10 -0400

    ejecutable

--- src/main/java/com/mycompany/galeria/de/arte/Main.java
+++ src/main/java/com/mycompany/galeria/de/arte/Main.java
@@ -1,5 +1,11 @@
 package com.mycompany.galeria.de.arte;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Galeria galeria = new Galeria();
+        ConsolaMenu menu = new ConsolaMenu(galeria);
+
+        menu.mostrarMenu();
+    }
 }
